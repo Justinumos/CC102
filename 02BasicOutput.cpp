3b1d5caee1730cc2716964e93c125308cf31c329
Justinumos <johnjustinlaudes@gmail.com>
2024-10-03 10:30:50 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+  
+    cout << "Our Lady Lourdes College Foundation" << endl;
+    cout << "College of computer  Studies" << endl;
+    cout << "Bachelor of Science  and information Technology 1-B" << endl;
+    cout << "John Justin G. Laudes" << endl;
+    cout << "Purok-6 Tulay na Lupa Labo Camarines Norte" << endl;
+    return 0;
+  }
